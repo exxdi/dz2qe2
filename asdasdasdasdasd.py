6d111ca59bcdc4de32f183da162fb455d9eb4a66
exxdi <144145087+exxdi@users.noreply.github.com>
2023-09-30 20:40:31 +0300

    Create asdasdasdasdasd.py

---
+++ asdasdasdasdasd.py
@@ -0,0 +1,8 @@
+import colorama
+from colorama import Fore, Back, Style, init, deinit
+print("Це найважливіші атрибути колорами і я їх опишу в readme")
+print("Fore.RED")
+print("Back.GREEN")
+print("Style.BRIGHT")
+print("init")
+print("deinit")
